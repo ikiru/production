d880f8948d399cb8c6610241780a49859525318e
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-10-25 20:54:34 -0500

    added sections

--- src/js/production.js
+++ src/js/production.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-  yourArray = ["Hello!", "Wonderful!", "You're the best!", "Hooray!"];
+  yourArray = ["Costumes", "Stage", "Production"];
 
   var options = {
     fadeInSpeed: 1000,
-    displayDuration: 2000,
+    displayDuration: 3000,
     fadeOutSpeed: 1000,
     delay: 500
   };
-
   $("#cycletext").cycleText(yourArray, options);
 });
